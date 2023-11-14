3bb1b7a47a473d6d069a096c3754da6a09ebf798
Mihail <110977245+mihaildernov@users.noreply.github.com>
2023-11-14 10:42:12 +0500

    Update Line_Follower_Angle.py

--- Line_Follower_Angle.py
+++ Line_Follower_Angle.py
@@ -7,7 +7,6 @@ import picamera
 from dronekit import *
 from pymavlink import mavutil
 import time
-import math
 
 master = mavutil.mavlink_connection('/dev/ttyACM0')
 vehicle = connect('/dev/ttyACM0', wait_ready=True)
@@ -28,27 +27,26 @@ def move_right(duration):
     vehicle.channels.overrides['3'] = a
     vehicle.channels.overrides['1'] = b
     time.sleep(duration)
-    vehicle.channels.overrides['3'] = 1100
+    vehicle.channels.overrides['3'] = 1400
     vehicle.channels.overrides['1'] = 1500
 
 def move_forward(duration):
     vehicle.channels.overrides['3'] = 1500
     time.sleep(duration)
-    vehicle.channels.overrides['3'] = 1100
+    vehicle.channels.overrides['3'] = 1400
 
 def move_left(duration):
     vehicle.channels.overrides['3'] = a
     vehicle.channels.overrides['1'] = c
     time.sleep(duration)
-    vehicle.channels.overrides['3'] = 1100
+    vehicle.channels.overrides['3'] = 1400
     vehicle.channels.overrides['1'] = 1500
 
 camera = picamera.PiCamera()
+camera.resolution = (192, 108)
 camera.framerate = 20
 camera.zoom = (0.1, 0.1, 0.6, 0.1)
-rawCapture = PiRGBArray(camera,size=(1920,1080))
-
-time.sleep(1)
+rawCapture = PiRGBArray(camera, size=(192, 108))
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -60,14 +58,14 @@ GPIO.setup(21, GPIO.OUT)
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
-    cv2.imshow('img',image)
+    cv2.imshow('img', image)
     key = cv2.waitKey(1) & 0xFF
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
     lines = cv2.HoughLines(edges, 1, math.pi / 180, 200)
-    
+
     if lines.any():
-        
+
         for line in lines:
             rho, theta = line[0]
             angle = math.degrees(theta)
@@ -80,9 +78,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
                 angle2 = angle
                 print("Угол между линией и центром:", angle)
 
-                a = 1400 + angle * 1,5
-                b = 1700 + angle * 1,5
-                c = 1350 - angle * 1,5
+                a = 1400 + angle * 2
+                b = 1700 + angle * 2
+                c = 1350 - angle * 2
 
                 if angle2 > -2:
                     GPIO.output(12, GPIO.LOW)
@@ -101,11 +99,10 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
                     GPIO.output(21, GPIO.LOW)
                     print("Поворот влево")
                     move_left(0.5)
-                    
+
     if key == ord("q"):
             break
-
-rawCapture.truncate(0)
+            rawCapture.truncate(0)
 
 GPIO.output(12, GPIO.LOW)
 GPIO.output(16, GPIO.LOW)
