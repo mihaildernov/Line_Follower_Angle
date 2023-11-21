af058083619dbdc1a26336ea248fe3295b0e2411
Mihail <110977245+mihaildernov@users.noreply.github.com>
2023-11-21 11:53:14 +0500

    Update Line_Follower_Angle.py

--- Line_Follower_Angle.py
+++ Line_Follower_Angle.py
@@ -6,6 +6,7 @@ from dronekit import *
 from pymavlink import mavutil
 import time
 import math
+import numpy as np
 
 master = mavutil.mavlink_connection('/dev/ttyACM0')
 vehicle = connect('/dev/ttyACM0', wait_ready=True)
@@ -17,32 +18,39 @@ while not vehicle.armed:
     print("Ждем моторы...")
     time.sleep(1) 
 
+def set_velocity(vehicle, vx, vy, vz):
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+    0, 0, 0, mavutil.mavlink.MAV_FRAME_LOCAL_NED, 0b0000111111000111, 0, 0, 0, vx, vy, vz, 0, 0, 0, 0, 0)
+    vehicle.send_mavlink(msg)
+
 def circle(duration):
     vehicle.channels.overrides['1'] = 1200
     time.sleep(duration)
     vehicle.channels.overrides['1'] = 1500
 
 def move_right(duration):
-    a = int(1400 + angle * 5)
-    b = int(1700 + angle * 5)
+    a = int(1450 + abs(angle) * 2)
+    b = int(1700 + abs(angle) * 2)
     vehicle.channels.overrides['3'] = a
     vehicle.channels.overrides['1'] = b
     time.sleep(duration)
-    vehicle.channels.overrides['3'] = 1400
+    vehicle.channels.overrides['3'] = 1200
     vehicle.channels.overrides['1'] = 1500
+
 
 def move_forward(duration):
     vehicle.channels.overrides['3'] = 1500
     time.sleep(duration)
-    vehicle.channels.overrides['3'] = 1400
+    vehicle.channels.overrides['3'] = 1200
+
 
 def move_left(duration):
-    a = int(1400 + angle * 5)
-    c = int(1350 - angle * 5)
+    a = int(1450 + abs(angle) * 2)
+    c = int(1350 - abs(angle) * 2)
     vehicle.channels.overrides['3'] = a
     vehicle.channels.overrides['1'] = c
     time.sleep(duration)
-    vehicle.channels.overrides['3'] = 1400
+    vehicle.channels.overrides['3'] = 1200
     vehicle.channels.overrides['1'] = 1500
 
 cap = cv2.VideoCapture(0)
@@ -55,58 +63,72 @@ GPIO.setup(16, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
 GPIO.setup(21, GPIO.OUT)
 
+set_velocity(vehicle, 0.1, 0.1, 0.1)
+
+vehicle.airspeed = 0.1
+
 while True:
     ret, image = cap.read()
     image = cv2.resize(image, (640, 480))
     height, width = image.shape[:2]
-    top_cutoff = int(height * 0.25)
-    bottom_cutoff = int(height * 0.75)
+    top_cutoff = int(height * 0.01)
+    bottom_cutoff = int(height * 0.66)
     image = image[top_cutoff:bottom_cutoff, :]
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-    lines = cv2.HoughLines(edges, 1, math.pi / 180, 200)
+    theta=0
+    minLineLength = 10
+    maxLineGap = 20
+    lower_blue = np.array([169, 100, 100])
+    upper_blue = np.array([189, 255, 255])
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    hsv = cv2.cvtColor(rgb, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    edges = cv2.Canny(mask, 50, 150)
+    cv2.imshow("img", edges)
+    if cv2.waitKey(1) >= 0:
+        break
+    lines = cv2.HoughLinesP(edges,1,np.pi/180,10,minLineLength,maxLineGap)
 
     if lines is not None:
 
         for line in lines:
-            rho, theta = line[0]
-            angle = int(math.degrees(theta))
+            x1, y1, x2, y2 = line[0]
+            dx = x2 - x1
+            dy = y2 - y1 
+            angle_in_radians = math.atan2(dy, dx)
+            angle = int(math.degrees(angle_in_radians))
 
-            if angle > 120:
-                angle = angle - 90
-                print(f"Отклонение от траектории на {-angle} градусов")
-
-                if angle < 10 and angle >= 0:
+            if angle < 0:
+                angle = angle + 90
+                print(f"Отклонение от траектории на {angle} градусов")
+                if (angle < 10 and angle >= 0) or (angle >= 70 and angle <= 90):
                     GPIO.output(12, GPIO.HIGH)
                     GPIO.output(21, GPIO.HIGH)
                     print("Движение прямо")
                     move_forward(0.5)
                 else:
-                    GPIO.output(12, GPIO.HIGH)
-                    GPIO.output(21, GPIO.LOW)
-                    print("Поворот влево")
-                    move_left(0.5)
-            else:
-                print(f"Отклонение от траектории на {angle} градусов")
-
-                if angle >= 10:
                     GPIO.output(12, GPIO.LOW)
                     GPIO.output(21, GPIO.HIGH)
                     print("Поворот вправо")
                     move_right(0.5)
 
-                if angle < 10 and angle >= 0:
+            else:
+                angle = angle - 90
+                print(f"Отклонение от траектории на {-angle} градусов")
+
+                if angle <= -10:
+                    GPIO.output(12, GPIO.HIGH)
+                    GPIO.output(21, GPIO.LOW)
+                    print("Поворот влево")
+                    move_left(0.5)
+
+                if angle > -10 and angle <= 0:
                     GPIO.output(12, GPIO.HIGH)
                     GPIO.output(21, GPIO.HIGH)
                     print("Движение прямо")
                     move_forward(0.5)
                     
-                else:
-                    print("Линия не найдена")
-
-    cv2.imshow("img", image)
-    if cv2.waitKey(1) >= 0:
-        break
+    else:
+        print("Линия не найдена")
 
 GPIO.output(12, GPIO.LOW)
 GPIO.output(16, GPIO.LOW)
